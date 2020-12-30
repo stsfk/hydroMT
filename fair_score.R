38b085005384475f74761c53cf7748a33ffed325
stsfk <yyang90@connect.hku.hk>
2020-12-30 19:45:37 +0800

    init fair score

---
+++ fair_score.R
@@ -0,0 +1,27 @@
+if (!require("pacman"))
+  install.packages("pacman")
+pacman::p_load(
+  tidyverse,
+  tidymodels,
+  caret,
+  lubridate,
+  zeallot,
+  xgboost,
+  cowplot,
+  doParallel,
+  Cubist,
+  pls,
+  earth,
+  elasticnet,
+  ipred,
+  plyr,
+  e1071,
+  kernlab,
+  randomForest,
+  ParBayesianOptimization,
+  xgboost,
+  lemon
+)
+
+
+
